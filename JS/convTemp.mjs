630e1cbbf5660bb6e5e470b4a3482fdb9b536fab
Thony <anthony.rita83@gmail.com>
2026-06-12 11:19:39 +0200

    presque fini

---
+++ JS/convTemp.mjs
@@ -0,0 +1,38 @@
+import {createInterface} from 'node:readline/promises';
+import {stdin as input,stdout as output, stderr} from 'node:process';
+
+const rl = new createInterface(input,output);
+
+async function main() 
+{
+    let test=[];
+    do
+    {
+        let rep =await rl.question('x\n> ');
+        rep = rep.toLowerCase();
+        test = rep.split(' ');
+    }while(!test.includes('c') && !test.includes('f'));
+
+    switch(test[1])
+    {
+        case 'c':
+            console.log(verF(test));
+            break;
+        case 'f':
+            console.log(verC(test));
+            break;
+    }
+    rl.close();
+}
+
+function verF(tableau)
+{
+    let result = (parseFloat(tableau[0])*9/5)+32;
+    return result;
+}
+function verC(tableau)
+{
+    let result = (parseFloat(tableau[0])-32)*5/9;
+    return result; 
+}
+main();
